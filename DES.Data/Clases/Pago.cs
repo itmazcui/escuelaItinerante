4d57acd97c3e6ec036aa459e7a61c1bfd85c38b0
it.mazcui <it.matiasazcui@gmail.com>
2019-03-02 11:20:35 -0300

    reporte pagos

---
+++ DES.Data/Clases/Pago.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DES.Data.Clases
+{
+    public class Pago
+    {
+        public int MontoAbonado { get; set; }
+        public DateTime FechaPago { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get; set; }
+        public Sede Sede  { get; set; }
+        public string TipoPago { get; set; }
+    }
+}
---
+++ DES.Logic/PagoLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DES.Data;
+using DES.Data.Clases;
+
+namespace DES.Logic
+{
+    public class PagoLogic
+    {
+        private readonly PagoData _alumnosData = new PagoData();
+
+        public IList<Pago> GetPagos(Sede sede, DateTime? fechaDesde, DateTime? fechaHasta)
+        {
+            var fechaDesdeSql = fechaDesde ?? (DateTime)SqlDateTime.MinValue;
+            var fechaHastaSql = fechaHasta ?? (DateTime)SqlDateTime.MaxValue;
+
+            return _alumnosData.GetPagos(sede, fechaDesdeSql, fechaHastaSql);
+        }
+    }
+}
